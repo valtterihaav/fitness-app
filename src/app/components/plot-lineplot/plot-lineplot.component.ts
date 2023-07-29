ebdade0a805b1f70d890ce39df204dd57e3da777
Valtteri Haavisto <vade.haavisto@gmail.com>
2023-07-29 15:02:43 +0300

    Small changes.

--- src/app/components/plot-lineplot/plot-lineplot.component.ts
+++ src/app/components/plot-lineplot/plot-lineplot.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, Input } from '@angular/core';
 import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
@@ -8,7 +8,8 @@ import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 })
 export class PlotLineplotComponent {
 
-  public lineChartOptions: ChartOptions = {
+  // chart options
+  lineChartOptions: ChartOptions = {
     responsive: true,
     plugins: {
       title: {
@@ -17,15 +18,14 @@ export class PlotLineplotComponent {
       }
     }
   };
-  public lineChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012']; // Mock labels
-  public lineChartType: ChartType = 'line';
-  public lineChartLegend = true;
-  public lineChartData: ChartDataset[] = [
+  lineChartType: ChartType = 'line';
+
+  // to be inputted to the component from the parent
+  @Input() lineChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012']; // Mock labels
+  @Input() lineChartLegend = true;
+  @Input() lineChartData: ChartDataset[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }, // Mock data
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }  // Mock data
   ];
-
-
-  ngOnInit() { }
 
 }
