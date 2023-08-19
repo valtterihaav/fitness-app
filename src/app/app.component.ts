e8fe115f02a4726354315cbd12edd035b15008e8
Valtteri Haavisto <vade.haavisto@gmail.com>
2023-08-19 12:07:12 +0300

    labels are not common

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,19 @@ import { ChartDataset } from 'chart.js';
 export class AppComponent {
   dataLineplot: ChartDataset[] = [];
   dataBarplot: ChartDataset[] = [];
-  commonLabels: string[] = [];
+  labelsLinePlot: string[] = [];
+  labelsBarPlot: string[] = [];
 
   setData(data_backend: any) {
     if ("no_data" in data_backend) {
       return;
     }
     this.dataLineplot = Array(data_backend["weight"]);
+    this.labelsLinePlot = data_backend["weight"]["xLabels"];
+
     this.dataBarplot = Array(data_backend["all_sports"]);
-    this.commonLabels = data_backend["common_label"];
-    console.log(this.dataLineplot, this.dataBarplot, this.commonLabels)
+    this.labelsBarPlot = data_backend["all_sports"]["xLabels"];
+
+    console.log(this.dataLineplot, this.dataBarplot, this.labelsLinePlot, this.labelsBarPlot)
   }
 }
